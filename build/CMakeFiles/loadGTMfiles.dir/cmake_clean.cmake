5ba2455a926ea9115e417ce05d361340e5e1779b
Mengxuexu <mengxuexu97@gmail.com>
2020-12-07 22:51:34 -0800

    updated

---
+++ build/CMakeFiles/loadGTMfiles.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loadGTMfiles.dir/src/main.cpp.o"
+  "loadGTMfiles"
+  "loadGTMfiles.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/loadGTMfiles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/loadGTMfilesLib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loadGTMfilesLib.dir/src/argvparser.cpp.o"
+  "CMakeFiles/loadGTMfilesLib.dir/src/loadGTMfiles.cpp.o"
+  "libloadGTMfilesLib.a"
+  "libloadGTMfilesLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/loadGTMfilesLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
